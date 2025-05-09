fcff15b4fc6409b4cb6ce1a59a2c8614cd5b7d23
DANUWA-MD <quizontalbot@gmail.com>
2025-05-09 18:17:13 +0530

    🔥 ＤＡＮＵＷＡ－ＭＤ 🔥

--- config.js
+++ config.js
@@ -11,6 +11,7 @@ ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 DANUWA-MD Is Alive Now😍*",
 MOVIE_API_KEY: process.env.MOVIE_API_KEY || "sky|51c173810f25c71beb51b431867187b736b89c28",
 GEMINI_API_KEY: process.env.GEMINI_API_KEY || "", // Paste your gemini API key
 BOT_OWNER: '94776121326',  // Replace with the owner's phone number
+AUTO_READ_STATUS: "true",
 
 
 
