adb51dd99aea57aec63fbdbba7fd9076c9b7a7ff
DANUWA-MD <quizontalbot@gmail.com>
2025-05-12 20:41:07 +0530

    🔥 ＤＡＮＵＷＡ－ＭＤ 🔥

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "", // Paste your session id here
-ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/DANUWA-MD/DANUWA-MD/blob/main/images/DANUWA-MD.png?raw=true",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/DANUWA-MD/DANUWA-BOT/blob/main/images/Danuwa%20-%20MD.png?raw=true",
 ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 DANUWA-MD Is Alive Now😍*",
 MOVIE_API_KEY: process.env.MOVIE_API_KEY || "sky|51c173810f25c71beb51b431867187b736b89c28",
 GEMINI_API_KEY: process.env.GEMINI_API_KEY || "", // Paste your gemini API key
