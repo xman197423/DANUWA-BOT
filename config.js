dab60d5d300628d5248c9e83faf39f623832f46a
Danuka DIssanayaka <quizontalbot@gmail.com>
2025-05-15 19:42:05 +0530

    🔥 ＤＡＮＵＷＡ－ＭＤ 🔥

--- config.js
+++ config.js
@@ -9,11 +9,9 @@ SESSION_ID: process.env.SESSION_ID || "", // Paste your session id here
 ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/DANUWA-MD/DANUWA-BOT/blob/main/images/Danuwa%20-%20MD.png?raw=true",
 ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 DANUWA-MD Is Alive Now😍*",
 MOVIE_API_KEY: process.env.MOVIE_API_KEY || "sky|51c173810f25c71beb51b431867187b736b89c28",
+OPENAI_API_KEY: process.env.OPENAI_API_KEY || "", // Paste your OPENNAI(gpt-4to-mini) key
 GEMINI_API_KEY: process.env.GEMINI_API_KEY || "", // Paste your gemini API key
 BOT_OWNER: '94776121326',  // Replace with the owner's phone number
 ownerNumber: ['94776121326'], // Replace with the owner's phone number
 AUTO_READ_STATUS: "true",
-
-
-
 };
