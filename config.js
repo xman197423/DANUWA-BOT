3311ddfd2d8e6299e5f4646e2099e96311745910
Danuka Dissanayaka <quizontalbot@gmail.com>
2025-05-18 21:54:05 +0530

    🔥 ＤＡＮＵＷＡ－ＭＤ 🔥

--- config.js
+++ config.js
@@ -5,6 +5,13 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 
+
+
+
+// Change following defaultConfig if you are deploying DANUWA-MD on github actions/github codespaces or replit...Railway users don't need to edit this
+
+
+
 const defaultConfig = {
   SESSION_ID: "", // Put your session id here
   ALIVE_IMG: "https://github.com/DANUWA-MD/DANUWA-BOT/blob/main/images/Danuwa%20-%20MD.png?raw=true",
@@ -13,10 +20,14 @@ const defaultConfig = {
   OPENAI_API_KEY: "",// Add OPENAI API key here
   GEMINI_API_KEY: "", // Add Gemini API key here
   REMOVE_BG_API_KEY:"", // Add removebg API key here
+  WEATHER_API_KEY: "", // Add your weather API key here
   BOT_OWNER: "94776121326", // Replace your bot owner number here with 94(country code)
   ownerNumber: ["94776121326"], // Replace your bot owner number here (same as bot owner number)
   AUTO_READ_STATUS: "true", // Turn on or off auto read status from here
 };
+
+
+//*******************************************************************************************************************************************************
 
 module.exports = {
   SESSION_ID: process.env.SESSION_ID || defaultConfig.SESSION_ID,
@@ -26,6 +37,7 @@ module.exports = {
   OPENAI_API_KEY: process.env.OPENAI_API_KEY || defaultConfig.OPENAI_API_KEY,
   GEMINI_API_KEY: process.env.GEMINI_API_KEY || defaultConfig.GEMINI_API_KEY,
   REMOVE_BG_API_KEY: process.env.REMOVE_BG_API_KEY || defaultConfig.REMOVE_BG_API_KEY, 
+  WEATHER_API_KEY: process.env.WEATHER_API_KEY || defaultConfig.WEATHER_API_KEY,
   BOT_OWNER: process.env.BOT_OWNER || defaultConfig.BOT_OWNER,
   ownerNumber: process.env.OWNER_NUMBER
     ? process.env.OWNER_NUMBER.split(",")
