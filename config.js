b05286a877cda2ca5ffaa7056e5d4dd189acc3c7
Danuka Dissanayaka <quizontalbot@gmail.com>
2025-05-18 21:46:41 +0530

    Update config.js

--- config.js
+++ config.js
@@ -4,14 +4,31 @@ if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env'
 function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
+
+const defaultConfig = {
+  SESSION_ID: "", // Put your session id here
+  ALIVE_IMG: "https://github.com/DANUWA-MD/DANUWA-BOT/blob/main/images/Danuwa%20-%20MD.png?raw=true",
+  ALIVE_MSG: "*Hello👋 DANUWA-MD Is Alive Now😍*", // Change alive msg from here
+  MOVIE_API_KEY: "",// Add skymantion API key here
+  OPENAI_API_KEY: "",// Add OPENAI API key here
+  GEMINI_API_KEY: "", // Add Gemini API key here
+  REMOVE_BG_API_KEY:"", // Add removebg API key here
+  BOT_OWNER: "94776121326", // Replace your bot owner number here with 94(country code)
+  ownerNumber: ["94776121326"], // Replace your bot owner number here (same as bot owner number)
+  AUTO_READ_STATUS: "true", // Turn on or off auto read status from here
+};
+
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "", // Paste your session id here
-ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/DANUWA-MD/DANUWA-BOT/blob/main/images/Danuwa%20-%20MD.png?raw=true",
-ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 DANUWA-MD Is Alive Now😍*",
-MOVIE_API_KEY: process.env.MOVIE_API_KEY || "sky|51c173810f25c71beb51b431867187b736b89c28",
-OPENAI_API_KEY: process.env.OPENAI_API_KEY || "", // Paste your OPENNAI(gpt-4to-mini) key
-GEMINI_API_KEY: process.env.GEMINI_API_KEY || "", // Paste your gemini API key
-BOT_OWNER: '94776121326',  // Replace with the owner's phone number
-ownerNumber: ['94776121326'], // Replace with the owner's phone number
-AUTO_READ_STATUS: "true",
+  SESSION_ID: process.env.SESSION_ID || defaultConfig.SESSION_ID,
+  ALIVE_IMG: process.env.ALIVE_IMG || defaultConfig.ALIVE_IMG,
+  ALIVE_MSG: process.env.ALIVE_MSG || defaultConfig.ALIVE_MSG,
+  MOVIE_API_KEY: process.env.MOVIE_API_KEY || defaultConfig.MOVIE_API_KEY,
+  OPENAI_API_KEY: process.env.OPENAI_API_KEY || defaultConfig.OPENAI_API_KEY,
+  GEMINI_API_KEY: process.env.GEMINI_API_KEY || defaultConfig.GEMINI_API_KEY,
+  REMOVE_BG_API_KEY: process.env.REMOVE_BG_API_KEY || defaultConfig.REMOVE_BG_API_KEY, 
+  BOT_OWNER: process.env.BOT_OWNER || defaultConfig.BOT_OWNER,
+  ownerNumber: process.env.OWNER_NUMBER
+    ? process.env.OWNER_NUMBER.split(",")
+    : defaultConfig.ownerNumber,
+  AUTO_READ_STATUS: convertToBool(process.env.AUTO_READ_STATUS, defaultConfig.AUTO_READ_STATUS),
 };
