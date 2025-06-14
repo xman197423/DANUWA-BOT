9844db510bb9f0170c62a13c009e15a77c1897a7
Danuka Dissanayaka <quizontalbot@gmail.com>
2025-06-14 23:48:06 +0530

    Update config.js

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ function convertToBool(text, fault = 'true') {
 
 
 const defaultConfig = {
-  SESSION_ID: "", // Put your session id here
+  SESSION_ID: "PVZ0CBIa#5noDbPXcBVl0jmyTp9yhXepdjkB1q4pDk8t9SRWp99A", // Put your session id here
   ALIVE_IMG: "https://github.com/DANUWA-MD/DANUWA-BOT/blob/main/images/Danuwa%20-%20MD.png?raw=true",
   ALIVE_MSG: "*Hello👋 DANUWA-MD Is Alive Now😍*", // Change alive msg from here
   OPENAI_API_KEY: "",// Add OPENAI API key here
